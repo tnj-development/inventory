f51f71c7a7642b2d2a0041fd1ffbaf493cacff11
Q4D1952K <97731242+Q4D1952K@users.noreply.github.com>
2022-09-01 10:04:53 +0700

    Update decay.lua

--- lj-inventory/server/decay.lua
+++ lj-inventory/server/decay.lua
@@ -1,6 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-local TimeAllowed = 60 * 60 * 24 * 1 -- Maths for 1 day dont touch its very important and could break everything
+local TimeAllowed = 60 * 2 -- Maths for 1 day dont touch its very important and could break everything
 function ConvertQuality(item)
 	local StartDate = item.created
     local DecayRate = QBCore.Shared.Items[item.name:lower()]["decay"] ~= nil and QBCore.Shared.Items[item.name:lower()]["decay"] or 0.0
@@ -18,7 +18,7 @@ function ConvertQuality(item)
     return percentDone
 end
 
-QBCore.Functions.CreateCallback('inventory:server:ConvertQuality', function(source, cb, inventory, other)
+QBCore.Functions.CreateCallback('inventory:server:ConvertQuality', function(source, cb, inventory, other, id)
     local src = source
     local data = {}
     local Player = QBCore.Functions.GetPlayer(src)
@@ -83,6 +83,114 @@ QBCore.Functions.CreateCallback('inventory:server:ConvertQuality', function(sour
                     end
                 end
             end
+        end
+    end
+if id then
+        if Gloveboxes[id] then
+            local GlobeBoxItems = GetOwnedVehicleGloveboxItems(stash)
+            for k, item in pairs(GlobeBoxItems) do
+                if item.created then
+                    if QBCore.Shared.Items[item.name:lower()]["decay"] ~= nil or QBCore.Shared.Items[item.name:lower()]["decay"] ~= 0 then
+                        if item.info then
+                            if type(item.info) == "string" then
+                                item.info = {}
+                            end
+                            if item.info.quality == nil then
+                                item.info.quality = 100
+                            end
+                        else
+                            local info = {quality = 100}
+                            item.info = info
+                        end
+                        local quality = ConvertQuality(item)
+                        if item.info.quality then
+                            if quality < item.info.quality then
+                                item.info.quality = quality
+                            end
+                        else
+                            item.info = {quality = quality}
+                        end
+                    else
+                        if item.info then 
+                            item.info.quality = 100
+                        else
+                            local info = {quality = 100}
+                            item.info = info
+                        end
+                    end
+                end
+            end
+            SaveOwnedGloveboxItems(id, GlobeBoxItems)
+        elseif Trunks[id] then
+            local trunkItems = GetOwnedVehicleItems(stash)
+            for k, item in pairs(trunkItems) do
+                if item.created then
+                    if QBCore.Shared.Items[item.name:lower()]["decay"] ~= nil or QBCore.Shared.Items[item.name:lower()]["decay"] ~= 0 then
+                        if item.info then
+                            if type(item.info) == "string" then
+                                item.info = {}
+                            end
+                            if item.info.quality == nil then
+                                item.info.quality = 100
+                            end
+                        else
+                            local info = {quality = 100}
+                            item.info = info
+                        end
+                        local quality = ConvertQuality(item)
+                        if item.info.quality then
+                            if quality < item.info.quality then
+                                item.info.quality = quality
+                            end
+                        else
+                            item.info = {quality = quality}
+                        end
+                    else
+                        if item.info then 
+                            item.info.quality = 100
+                        else
+                            local info = {quality = 100}
+                            item.info = info
+                        end
+                    end
+                end
+            end
+            SaveOwnedVehicleItems(id, trunkItems)
+        elseif Stashes[id] then
+            local stashItems = GetStashItems(stash)
+            for k, item in pairs(stashItems) do
+                if item.created then
+                    if QBCore.Shared.Items[item.name:lower()]["decay"] ~= nil or QBCore.Shared.Items[item.name:lower()]["decay"] ~= 0 then
+                        if item.info then
+                            if type(item.info) == "string" then
+                                item.info = {}
+                            end
+                            if item.info.quality == nil then
+                                item.info.quality = 100
+                            end
+                        else
+                            local info = {quality = 100}
+                            item.info = info
+                        end
+                        local quality = ConvertQuality(item)
+                        if item.info.quality then
+                            if quality < item.info.quality then
+                                item.info.quality = quality
+                            end
+                        else
+                            item.info = {quality = quality}
+                        end
+                    else
+                        if item.info then 
+                            item.info.quality = 100
+                        else
+                            local info = {quality = 100}
+                            item.info = info
+                        end
+                    end
+                end
+            end
+            SaveStashItems(stash, stashItems)
         end
     end
     Player.Functions.SetInventory(inventory)
